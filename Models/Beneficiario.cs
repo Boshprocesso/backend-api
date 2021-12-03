e7e85001d6a5dd88c37ff56cc69b745c578ecf22
gustavohgmartins <martins.gustavohg@gmail,com>
2021-12-03 19:33:40 -0300

    initial commit

---
+++ Models/Beneficiario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace boschBeneficios.Models
+{
+    public partial class Beneficiario
+    {
+        public Guid IdBeneficiario { get; set; }
+        public string NomeCompleto { get; set; } = null!;
+        public DateTime? DataNascimento { get; set; }
+        public int? Edv { get; set; }
+        public string? Cpf { get; set; }
+        public string? Unidade { get; set; }
+        public DateTime? DataInclusao { get; set; }
+        public string? ResponsavelInclusao { get; set; }
+    }
+}
